fb36b7c39e2a6abe457ef0fe9996764b116497bd
Joseph Calla <josephestanislao9@gmail.com>
2022-10-19 07:33:21 -0500

    NOTICKET: Setting Up Firebase

--- Instagram-App/Managers/AuthManager.swift
+++ Instagram-App/Managers/AuthManager.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+final class AuthManager {
+    static let shared = AuthManager()
+    
+    let auth = Auth.auth()
+    
+    private init() {}
+    
+}
