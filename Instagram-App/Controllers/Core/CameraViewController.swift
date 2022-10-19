2ba2e527d516546ce3d7910cf5e6ed6aa1d76292
Joseph Calla <josephestanislao9@gmail.com>
2022-10-19 08:13:11 -0500

    NOTICKET: Set Up Managers

--- Instagram-App/Controllers/Core/CameraViewController.swift
+++ Instagram-App/Controllers/Core/CameraViewController.swift
@@ -11,8 +11,9 @@ class CameraViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Camera"
+        view.backgroundColor = .systemBackground
+        
     }
     
 
