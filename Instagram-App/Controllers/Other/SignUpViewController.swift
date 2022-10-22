7836d763e4e152d858fdd71b9d695014e8370f51
Joseph Calla <josephestanislao9@gmail.com>
2022-10-22 10:45:38 -0500

    NOTICKET: Sign Up UI

--- Instagram-App/Controllers/Other/SignUpViewController.swift
+++ Instagram-App/Controllers/Other/SignUpViewController.swift
@@ -6,13 +6,251 @@
 //
 
 import UIKit
+import SafariServices
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    // Subviews
+    private let profilePictureImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.tintColor = .lightGray
+        imageView.image = UIImage(systemName: "person.circle")
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 45
+        return imageView
+    }()
+    private let usernameField: IGTextField = {
+        let field = IGTextField()
+        field.placeholder = "Username"
+        field.returnKeyType = .next
+        field.autocorrectionType = .no
+        return field
+    }()
+    private let emailField: IGTextField = {
+        let field = IGTextField()
+        field.placeholder = "Email Address"
+        field.keyboardType = .emailAddress
+        field.returnKeyType = .next
+        field.autocorrectionType = .no
+        return field
+    }()
+    
+    private let passwordField: IGTextField = {
+        let field = IGTextField()
+        field.placeholder = "Create Password"
+        field.keyboardType = .default
+        field.returnKeyType = .continue
+        field.autocorrectionType = .no
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let signInButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Sign Up", for: .normal)
+        button.backgroundColor = .systemGreen
+        button.layer.cornerRadius = 8
+        button.layer.masksToBounds = true
+        return button
+    }()
+     
+    private let termsButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Terms of Service", for: .normal)
+        button.setTitleColor(.link, for: .normal)
+        return button
+    }()
+    
+    private let privacyButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Privacy Policity", for: .normal)
+        button.setTitleColor(.link, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Sign Up"
+        view.backgroundColor = .systemBackground
+        addSubviews()
+        
+        usernameField.delegate = self
+        emailField.delegate = self
+        passwordField.delegate = self
+        
+        addButtonActions()
+        addImageGesture()
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let imageSize: CGFloat = 90
+        
+        profilePictureImageView.frame = CGRect(x: 0,
+                                               y: view.safeAreaInsets.top,
+                                               width: imageSize,
+                                               height: imageSize)
+        
+        usernameField.frame = CGRect(x: 25,
+                                     y: profilePictureImageView.buttom+20,
+                                     width: view.width-50,
+                                     height: 50)
+        
+        emailField.frame = CGRect(x: 25,
+                                  y: usernameField.buttom+20,
+                                  width: view.width-50,
+                                  height: 50)
+        
+        passwordField.frame = CGRect(x: 25,
+                                     y: emailField.buttom+20,
+                                     width: view.width-50,
+                                     height: 50)
+        
+        signInButton.frame = CGRect(x: 25,
+                                    y: passwordField.buttom+20,
+                                    width: view.width-50,
+                                    height: 50)
+        
+        
+        termsButton.frame = CGRect(x: 25,
+                                   y: signInButton.buttom+50,
+                                   width: view.width-50,
+                                   height: 50)
+        
+        privacyButton.frame = CGRect(x: 25,
+                                     y: termsButton.buttom,
+                                     width: view.width-50,
+                                     height: 50)
+    }
+    
+    
+    private func addSubviews() {
+        view.addSubview(profilePictureImageView)
+        view.addSubview(usernameField)
+        view.addSubview(emailField)
+        view.addSubview(passwordField)
+        view.addSubview(signInButton)
+        view.addSubview(termsButton)
+        view.addSubview(privacyButton)
+    }
+    
+    private func addButtonActions() {
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        termsButton.addTarget(self, action: #selector(didTapTerms), for: .touchUpInside)
+        privacyButton.addTarget(self, action: #selector(didTapPrivacy), for: .touchUpInside)
+    }
+    
+    private func addImageGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapImage))
+        profilePictureImageView.isUserInteractionEnabled = true
+        profilePictureImageView.addGestureRecognizer(tap)
+    }
+    
+    // MARK: - Actions
+    @objc func didTapSignIn() {
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+        
+        guard let username = usernameField.text,
+              let email = emailField.text,
+              let password = passwordField.text,
+              !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty,
+              !username.trimmingCharacters(in: .whitespaces).isEmpty,
+              password.count >= 6,
+              username.count >= 2,
+              username.trimmingCharacters(in: .alphanumerics).isEmpty else {
+            presentError()
+            return
+        }
+        
+        // Sign in with authManager
+        let vc = HomeViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func presentError() {
+        let alert = UIAlertController(title: "Woops",
+                                      message: "Please make sure to fill all fields and have a password lonver that 6 characters",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+        present(alert, animated: true)
+    }
+    
+    @objc func didTapTerms() {
+        guard let url = URL(string: "https://www.instagram.com") else {
+            return
+        }
+        
+        let vc = SFSafariViewController(url: url)
+        present(vc, animated: true)
+    }
+    
+    @objc func didTapPrivacy() {
+        guard let url = URL(string: "https://www.instagram.com") else {
+            return
+        }
+        
+        let vc = SFSafariViewController(url: url)
+        present(vc, animated: true)
+    }
+    
+    @objc func didTapImage() {
+        let sheet = UIAlertController(title: "Profile Picture", message: "Set a picture to help your friends find you.", preferredStyle: .actionSheet)
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        sheet.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { [weak self] _ in
+            DispatchQueue.main.async {
+                let picker = UIImagePickerController()
+                picker.sourceType = .camera
+                picker.allowsEditing = true
+                picker.delegate = self
+                self?.present(picker, animated: true)
+            }
+        }))
+        
+        sheet.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: { [weak self] _ in
+            DispatchQueue.main.async {
+                let picker = UIImagePickerController()
+                picker.allowsEditing = true
+                picker.sourceType = .photoLibrary
+                picker.delegate = self
+                self?.present(picker, animated: true)
+            }
+        }))
+        present(sheet, animated: true)
+        
+    }
+    
+    // MARK: - Field Delegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == usernameField {
+            emailField.becomeFirstResponder()
+        } else if textField == emailField {
+            passwordField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+            didTapSignIn()
+        }
+        return true
+    }
+    // Image Picker
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true)
+
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            return
+        }
+        
+        profilePictureImageView.image = image
+    }
 }
+
