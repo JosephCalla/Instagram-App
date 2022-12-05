f0c6ab14eaeb106bd47ff8f1c27673f0d13c7a66
Joseph Calla <35270796+JosephCalla@users.noreply.github.com>
2022-12-04 22:06:50 -0500

    Home Feed: Customizing Home Cells

---
+++ Instagram-App/Views/Home Cells/PostCollectionViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  PostCollectionViewCell.swift
+//  Instagram-App
+//
+//  Created by Joseph Estanislao Calla Moreyra on 4/12/22.
+//
+
+import SDWebImage
+import UIKit
+
+class PostCollectionViewCell: UICollectionViewCell {
+    static let identifier = "PostCollectionViewCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.clipsToBounds = true
+        contentView.backgroundColor = .secondarySystemBackground
+        contentView.addSubview(imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+    
+    func configure(with viewModel: PostCollectionViewCellViewModel) {
+        imageView.sd_setImage(with: viewModel.postURL)
+    }
+}
+
+
