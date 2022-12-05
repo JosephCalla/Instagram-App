6764d898c27c8a8c67dd6b4b8d2caaefe97cfdeb
Joseph Calla <35270796+JosephCalla@users.noreply.github.com>
2022-12-05 15:56:36 -0500

    Camera: Create Custom Camera

--- Instagram-App/Controllers/Core/CameraViewController.swift
+++ Instagram-App/Controllers/Core/CameraViewController.swift
@@ -6,25 +6,107 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
+    
+    private var output = AVCapturePhotoOutput()
+    private var captureSession: AVCaptureSession?
+    private let previewLayer = AVCaptureVideoPreviewLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Camera"
-        view.backgroundColor = .systemBackground
-        
+        view.backgroundColor = .black
+        title = "Take Photo"
+        setupNavBar()
+        checkCameraPermission()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        tabBarController?.tabBar.isHidden = true
     }
-    */
-
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        captureSession?.stopRunning()
+        //
+        //        if let session = captureSession, !session.isRunning {
+        //            session.startRunning()
+        //        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        previewLayer.frame = CGRect(
+            x: 0,
+            y: view.safeAreaInsets.top,
+            width: view.width,
+            height: view.width
+        )
+    }
+    
+    @objc func didTapClose() {
+        tabBarController?.selectedIndex = 0
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    private func setupNavBar() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close,
+                                                           target: self,
+                                                           action: #selector(didTapClose))
+        
+        //        navigationController?.navigationBar.setBackgroundImage(UIImage(),
+        //                                                               for: UIBarMetrics.default)
+        //        navigationController?.navigationController?.navigationBar.shadowImage = UIImage()
+        //        navigationController?.navigationBar.backgroundColor = .clear
+    }
+    
+    private func checkCameraPermission() {
+        switch AVCaptureDevice.authorizationStatus(for: AVMediaType.video) {
+        case .notDetermined:
+            // Request
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                guard granted else {
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self.setUpCamera()
+                }
+            }
+        case .restricted,.denied:
+            break
+        case .authorized:
+            setUpCamera()
+        @unknown default:
+            break
+        }
+    }
+    
+    private func setUpCamera() {
+        let captureSession = AVCaptureSession()
+        if let device = AVCaptureDevice.default(for: .video) {
+            do {
+                let input = try AVCaptureDeviceInput(device: device)
+                if captureSession.canAddInput(input) {
+                    captureSession.addInput(input)
+                }
+            } catch {
+                print(error)
+            }
+            
+            if captureSession.canAddOutput(output) {
+                captureSession.addOutput(output)
+            }
+            
+            // Layer
+            previewLayer.session = captureSession
+            previewLayer.videoGravity = .resizeAspectFill
+            view.layer.addSublayer(previewLayer)
+            
+            captureSession.startRunning()
+        }
+    }
+    
 }
