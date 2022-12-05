97b28fee49ad3043f5b28bbce3262f89c07654d1
Joseph Calla <35270796+JosephCalla@users.noreply.github.com>
2022-12-05 01:14:46 -0500

    Home Feed: Wrap Up home cells

--- Instagram-App/Views/Home Cells/PostCaptionCollectionViewCell.swift
+++ Instagram-App/Views/Home Cells/PostCaptionCollectionViewCell.swift
@@ -10,10 +10,18 @@ import UIKit
 class PostCaptionCollectionViewCell: UICollectionViewCell {
     static let identifier = "PostCaptionCollectionViewCell"
     
+    private let label: UILabel = {
+       let label = UILabel()
+        label.numberOfLines = 0
+        label.textColor = .label
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentView.clipsToBounds = true
         contentView.backgroundColor = .systemBackground
+        contentView.addSubview(label)
     }
     
     required init?(coder: NSCoder) {
@@ -22,13 +30,17 @@ class PostCaptionCollectionViewCell: UICollectionViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        let size = label.sizeThatFits(CGSize(width: contentView.bounds.size.width-12,
+                                             height: contentView.bounds.size.height))
+        label.frame = CGRect(x: 12, y: 3, width: size.width, height: size.height)
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        label.text = nil
     }
     
     func configure(with viewModel: PostCaptionCollectionViewCellViewModel) {
-        
+        label.text = "\(viewModel.username): This is an awesome first post! \(viewModel.caption ?? "") "
     }
 }
