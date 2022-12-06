8b1ec47ac1414697130e730828fea448f9571e6b
Joseph Calla <35270796+JosephCalla@users.noreply.github.com>
2022-12-06 00:31:12 -0500

    Camera: Adding Caption

--- Instagram-App/Controllers/Other/CaptionViewController.swift
+++ Instagram-App/Controllers/Other/CaptionViewController.swift
@@ -7,9 +7,25 @@
 
 import UIKit
 
-class CaptionViewController: UIViewController {
+class CaptionViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
 
     private let image: UIImage
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let textView: UITextView = {
+       let textView = UITextView()
+        textView.text = "Add caption..."
+        textView.backgroundColor = .secondarySystemBackground
+        textView.font = .systemFont(ofSize: 22)
+        textView.textContainerInset = UIEdgeInsets(top: 3, left: 3, bottom: 3, right: 3)
+        return textView
+    }()
     
     init(image: UIImage) {
         self.image = image
@@ -23,7 +39,46 @@ class CaptionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        view.addSubview(imageView)
+        imageView.image = image
+        view.addSubview(textView)
+        textView.delegate = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Post", style: .done, target: self, action: #selector(didTapPost))
+    }
+    
+    @objc func didTapPost() {
+        textView.resignFirstResponder()
+        var caption = textView.text ?? ""
+        if caption == "Add caption..." {
+            caption = ""
+        }
+        
+        // Update photo, update database
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let size: CGFloat = view.width/4
+        imageView.frame = CGRect(
+            x: (view.width-size)/2,
+            y: view.safeAreaInsets.top + 10,
+            width: size,
+            height: size
+        )
+        
+        textView.frame = CGRect(
+            x: 20,
+            y: imageView.buttom+20,
+            width: view.width-40,
+            height: 100
+        )
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textView.text == "Add caption..." {
+            textView.text = nil
+        }
     }
 }
